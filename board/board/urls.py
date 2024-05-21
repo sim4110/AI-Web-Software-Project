6361be7db4a63bb01f949a7bc818df10d1fbc7d9
KSM017 <84719857+KSM017@users.noreply.github.com>
2024-05-21 16:18:10 +0900

    이미지 첨부 및 출력 기능 , 카테고리에 따라 목록 분류

---
+++ board/board/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'board'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list/', views.list, name='list'),
+    path('list_walk/', views.list_walk, name='list_walk'),
+    path('list_wash/', views.list_wash, name='list_wash'),
+    path('list_grooming/', views.list_grooming, name='list_grooming'),
+    path('read/<int:id>/', views.read, name='read'),
+    path('regist/', views.regist, name='regist'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('remove/<int:id>/', views.remove, name='remove'),
+]
